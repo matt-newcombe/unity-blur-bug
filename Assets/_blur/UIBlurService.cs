c09f7da1734ce4e9a608265655f3c2bce8a5414a
Matthew Newcombe <a@mnewcombe.com>
2022-07-26 14:57:16 +0100

    update

--- Assets/_blur/UIBlurService.cs
+++ Assets/_blur/UIBlurService.cs
@@ -10,31 +10,25 @@ namespace Cauldron.Exodus.Service
 {
     public class UIBlurService : MonoBehaviour
     {
+        public GraphicsFormat gfxFormat = GraphicsFormat.B10G11R11_UFloatPack32;
         public RawImage RawImage;
-        public GraphicsFormat GraphicsFormat = GraphicsFormat.B10G11R11_UFloatPack32;
+        public Camera Camera;
 
         [NonSerialized]
         public RenderTexture ScreenOverlayBlurTexture;
 
-        public Camera Camera;
-        public void Start()
-        {
-            ScreenOverlayBlurTexture = new RenderTexture(Screen.width, Screen.height, 16, GraphicsFormat.B10G11R11_UFloatPack32);
-            RawImage.texture = ScreenOverlayBlurTexture;
-        }
 
         public void DoBlur()
         {
-            ScreenOverlayBlurTexture = new RenderTexture(Screen.width, Screen.height, 16, GraphicsFormat.B10G11R11_UFloatPack32);
+            ScreenOverlayBlurTexture = new RenderTexture(Screen.width, Screen.height, 16, gfxFormat);
             RawImage.texture = ScreenOverlayBlurTexture;
-            KawaseCamBlur blurFeature = GetKawaseBlur(Camera);
+
+            KawaseCamBlur blurFeature = GetKawaseBlurRenderFeature(Camera);
             blurFeature.settings.toRenderTexture = ScreenOverlayBlurTexture;
-          //  blurFeature.settings.DisableAfterRender = true;
             blurFeature.SetActive(true);
-            RawImage.texture = ScreenOverlayBlurTexture;
         }
 
-        private KawaseCamBlur GetKawaseBlur(Camera cam)
+        private KawaseCamBlur GetKawaseBlurRenderFeature(Camera cam)
         {
             ScriptableRenderer scriptableRenderer = cam.GetUniversalAdditionalCameraData().scriptableRenderer;
             var property = typeof(ScriptableRenderer).GetProperty("rendererFeatures", BindingFlags.NonPublic | BindingFlags.Instance);
