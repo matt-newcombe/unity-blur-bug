62f23488ca9e236f46724b90ba03ba166b6cd5ba
Matthew Newcombe <a@mnewcombe.com>
2022-07-26 13:00:10 +0100

    [UIBlur] Blur project

---
+++ Assets/_blur/UIBlurService.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
+namespace Cauldron.Exodus.Service
+{
+    public class UIBlurService : MonoBehaviour
+    {
+        public RawImage RawImage;
+
+        [NonSerialized]
+        public RenderTexture ScreenOverlayBlurTexture;
+
+        public void Start()
+        {
+            ScreenOverlayBlurTexture = new RenderTexture(Screen.width, Screen.height, 16, GraphicsFormat.B10G11R11_UFloatPack32);
+            RawImage.texture = ScreenOverlayBlurTexture;
+        }
+
+        public void DoBlur()
+        {
+            KawaseCamBlur blurFeature = GetKawaseBlur(Camera.main);
+            blurFeature.settings.toRenderTexture = ScreenOverlayBlurTexture;
+            blurFeature.settings.DisableAfterRender = true;
+            blurFeature.SetActive(true);
+            RawImage.texture = ScreenOverlayBlurTexture;
+        }
+
+        private KawaseCamBlur GetKawaseBlur(Camera cam)
+        {
+            ScriptableRenderer scriptableRenderer = cam.GetUniversalAdditionalCameraData().scriptableRenderer;
+            var property = typeof(ScriptableRenderer).GetProperty("rendererFeatures", BindingFlags.NonPublic | BindingFlags.Instance);
+            List<ScriptableRendererFeature> features = property.GetValue(scriptableRenderer) as List<ScriptableRendererFeature>;
+            KawaseCamBlur blurFeature = null;
+
+            foreach (ScriptableRendererFeature feature in features)
+            {
+                if (feature is KawaseCamBlur blur)
+                {
+                    blurFeature = blur;
+                }
+            }
+
+            return blurFeature;
+        }
+    }
+}
